addbfe09de41346e53ccad58ed764797e62f18cd
Roman Pronskyi <wildnoblebrute@gmail.com>
2021-11-06 12:29:52 +0200

    init clear project

---
+++ practice_tabBar/practice_tabBar/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  practice_tabBar
+//
+//  Created by Роман Пронский on 06.11.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
